f9f27499361ec090a479e312badb1d87618d5fec
marwincn <qiangbo.xu@gmail.com>
2020-10-02 13:26:46 +0800

    合并简化Spring配置

--- src/main/java/cn/marwin/adminsystem/Application.java
+++ src/main/java/cn/marwin/adminsystem/Application.java
@@ -2,12 +2,59 @@ package cn.marwin.adminsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 开启Swagger文档
+     *
+     * @return Bean
+     */
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder().title("AdminSystem API文档").description("").version("develop-version").build())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.marwin.adminsystem.control"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 开启跨域支持
+     *
+     * @return Bean
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", getCorsConfiguration()); // 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 允许任何方法
+        corsConfiguration.setAllowCredentials(true); // 允许携带cookie
+        return corsConfiguration;
     }
 
 }
